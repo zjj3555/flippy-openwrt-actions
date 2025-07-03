6d3bb369b88be8e0c5cafe59dd6bdf9a986a76bc
zjj3555 <59527179+zjj3555@users.noreply.github.com>
2025-07-03 10:46:33 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#===============================================
+# Description: DIY script
+# File name: diy-script.sh
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#===============================================
+
+# 修改版本为编译日期，数字类型。
+date_version=$(date +"%Y%m%d%H")
+echo $date_version > version
+
+# 为iStoreOS固件版本加上编译作者
+author="zjj3555"
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version} by ${author}'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}\"/g" package/base-files/files/usr/lib/os-release
+
+# 拉取我的软件包仓库
+echo 'src-git xmpackages https://github.com/xiaomeng9597/openwrt-packages2.git;main' >> feeds.conf.default
